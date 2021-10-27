63e3f1a809938975666f161369261f285d2d0034
roman7501 <64649303+roman7501@users.noreply.github.com>
2021-10-27 15:41:59 +0200

    all horizontal

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const titleSVG = document.querySelector(".title-svg");
 const pathEls = titleSVG.querySelectorAll("path");
 const groupSVG = document.getElementById("LA_SCENE_EST_VIDE");
 
+const rectsEl = document.querySelector(".rects");
 let isPlay = false;
 
 btnEl.addEventListener("click", () => {
@@ -24,31 +25,52 @@ btnEl.addEventListener("click", () => {
 });
 
 /**
+ *
+ * Functions
+ */
+const createRectangles = (num) => {
+  const rectsElBoundings = rectsEl.getBoundingClientRect();
+  const newSVG = document.createElementNS(svgns, "svg");
+  for (let i = 0; i < num; i++) {
+    const rect = document.createElementNS(svgns, "rect");
+    const width = 10 * Math.random();
+    const x = (rectsElBoundings.width / num) * i;
+    const y = 0;
+    const height = rectsElBoundings.height;
+    const color = "#FBFAF5";
+
+    rect.setAttributeNS(null, "id", "rect");
+    rect.setAttributeNS(null, "x", x);
+    rect.setAttributeNS(null, "y", y);
+    rect.setAttributeNS(null, "width", width);
+    rect.setAttributeNS(null, "height", height);
+
+    rect.style.fill = color;
+    rect.style.transformOrigin = "center top";
+    newSVG.appendChild(rect);
+  }
+
+  rectsEl.appendChild(newSVG);
+};
+
+/**
  * Init
  */
 const svgns = "http://www.w3.org/2000/svg";
 const tl = gsap.timeline({ paused: true });
+const tlRect = gsap.timeline({ paused: true });
 
-titleEl.style.transform = "rotate(180deg)";
+// titleEl.style.transform = "rotate(180deg)";
 
+// Create one rectangle per letter
 pathEls.forEach((path, i) => {
   path.style.opacity = 0;
-  // CIRCLE
-  // const point = document.createElementNS(svgns, "circle");
-  // const x = 80 + 30 * i;
-  // const color = "#FBFAF5";
-  // point.setAttributeNS(null, "id", "points");
-  // point.setAttributeNS(null, "cx", x);
-  // point.setAttributeNS(null, "cy", 100);
-  // point.setAttributeNS(null, "r", 10);
-  // point.style.fill = color;
 
-  // RECT
   const point = document.createElementNS(svgns, "rect");
   const x = -20 + 40 * i;
-  const y = -130;
+  const y = -30;
   const width = 50;
-  const height = 400;
+  const height = 200;
   const color = "#FBFAF5";
   point.setAttributeNS(null, "id", "points");
   point.setAttributeNS(null, "x", x);
@@ -63,56 +85,103 @@ pathEls.forEach((path, i) => {
   MorphSVGPlugin.convertToPath("#points");
 });
 
-const pointsSVG = document.querySelectorAll("#points");
+// Create one rectangle per letter
+createRectangles(15);
 
+const pointsSVG = document.querySelectorAll("#points");
+const rectSVG = document.querySelectorAll("#rect");
 /**
  * Anim
  */
+// Anim Clip Path
 gsap.set(titleEl, {
-  clipPath: "polygon(50% 0%, 51% 0%, 51% 0%, 50% 0%)",
-  // clip-path: polygon(50% 45%, 51% 45%, 51% 46%, 50% 46%);
+  // clipPath: "polygon(0% 54%, 100% 54%, 100% 56%, 0% 56%)",
+  clipPath: "polygon(0% 54%, 0% 54%, 0% 55%, 0% 55%)",
 });
 tl.to(titleEl, {
-  clipPath: "polygon(50% 0%, 51% 0%, 51% 100%, 50% 100%)",
-  duration: 10,
-  ease: "power1.inOut",
+  clipPath: "polygon(0% 54%, 100% 54%, 100% 55%, 0% 55%)",
+  duration: 15,
+  ease: "linear",
 });
 tl.to(
   titleEl,
   {
     clipPath: " polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-    duration: 15,
-    delay: 10,
-    ease: "power2.in",
+    duration: 30,
+    delay: 15,
+    ease: "power1.in",
   },
   "<"
 );
-pointsSVG.forEach((point, i) => {
-  // PART 1
 
-  tl.to(point, { duration: 15, scaleX: 0.2, delay: 1, ease: "power1.inOut" }, "<");
-  setTimeout(() => {
-    tl.to(point, { duration: 20, morphSVG: pathEls[i], delay: 0.4, ease: "power1.inOut" }, "<");
-    tl.to(point, { duration: 20, scaleX: 1, delay: 0.4 }, "<");
-  }, 10000);
-});
-tl.to(titleEl, { duration: 30, rotation: 0, delay: 0, ease: "power1.inOut" }, "<");
-tl.to(titleEl, { duration: 30, scale: 0.6, delay: 0, ease: "power2.inOut" }, "<");
+// Anim Points
+tl.to("#points", { duration: 15, scaleX: 0.2, stagger: 1, ease: "power1.inOut" }, "<");
+tl.to("#points", { duration: 25, morphSVG: (i) => pathEls[i], delay: 10, stagger: 0.4, ease: "power1.inOut" }, "<");
+tl.to("#points", { duration: 20, scaleX: 1, stagger: 0.4 }, "<");
 
-tl.fromTo(
-  titleSVG,
-  {},
+// pointsSVG.forEach((point, i) => {
+//   tl.to(point, { duration: 15, scaleX: 0.2, delay: 1, ease: "power1.inOut" }, "<");
+//   setTimeout(() => {
+//     tl.to(point, { duration: 17, morphSVG: pathEls[i], delay: 0.4, ease: "power1.inOut" }, "<");
+//     tl.to(point, { duration: 20, scaleX: 1, delay: 0.4 }, "<");
+//   }, 10000);
+// });
+
+//Anim Rects
+gsap.set(
+  "#rect",
   {
-    duration: 40,
+    scaleX: 0,
+    duration: 2,
+    onStart: () => {
+      console.log("start");
+    },
+    onUpdate: () => {
+      console.log("change");
+    },
   },
   "<"
 );
 
-let tlLaunched = false;
+tlRect.to("#rect", {
+  scaleX: 1,
+  duration: "random(10, 20)",
+  delay: 12,
+  stagger: "random(-10, 10)",
+  ease: "power1.inOut",
+});
+tlRect.to(
+  "#rect",
+  {
+    x: "random(-400, 400)",
+    duration: "random(80, 90)",
+    delay: 12,
+    stagger: "random(-10, 10)",
+    ease: "power1.inOut",
+  },
+  "<"
+);
+tlRect.to(
+  "#rect",
+  {
+    scaleY: 0,
+    duration: "random(20, 26)",
+    delay: 5,
+    stagger: "random(-1, 1)",
+    ease: "power1.inOut",
+  },
+  "<"
+);
+
+// Anim Containers
+// tl.to(titleEl, { duration: 30, rotation: 0, delay: 0, ease: "power1.inOut" }, "<");
+tl.to(titleEl, { duration: 30, scale: 0.6, delay: 0, ease: "power2.inOut" }, "<");
 
 sonGenerique.addEventListener("pause", () => {
   tl.pause();
+  tlRect.pause();
 });
 sonGenerique.addEventListener("play", () => {
   tl.play();
+  tlRect.play();
 });
